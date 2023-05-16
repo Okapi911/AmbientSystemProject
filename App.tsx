026e79df64bcff16a346c4b780c90f6d1207014a
Okapi911 <lucas.kloubert5@etu.univ-lorraine.fr>
2023-05-16 08:55:46 +0200

    firebase implementation 1

--- App.tsx
+++ App.tsx
@@ -1,3 +1,46 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDtKiDviLvkSvGT5xwFtuC2fo-VF9GxZHA",
+
+  authDomain: "ambientsystemsqr.firebaseapp.com",
+
+  projectId: "ambientsystemsqr",
+
+  storageBucket: "ambientsystemsqr.appspot.com",
+
+  messagingSenderId: "573952725334",
+
+  appId: "1:573952725334:web:b19779484ae95e8c0ec6f7",
+
+  measurementId: "G-V8PV5JYXG3"
+
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDtKiDviLvkSvGT5xwFtuC2fo-VF9GxZHA",
+    authDomain: "ambientsystemsqr.firebaseapp.com",
+    projectId: "ambientsystemsqr",
+    storageBucket: "gs://ambientsystemsqr.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+  };
+  
